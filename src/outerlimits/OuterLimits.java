32370742b7eacb6069c7bbc67428036dfbc3d539
copperTones <evilcreeper64@gmail.com>
2025-01-09 16:28:39 -0600

    template housekeeping

---
+++ src/outerlimits/OuterLimits.java
@@ -0,0 +1,23 @@
+package outerlimits;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class OuterLimits extends Mod{
+
+	public OuterLimits(){
+		Log.info("Loaded ExampleJavaMod constructor.");
+	}
+
+	@Override
+	public void loadContent(){
+		Log.info("Loading some example content.");
+	}
+
+}
