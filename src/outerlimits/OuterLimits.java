c577fa945f2d1096fc8677c2d98d7678358004c4
copperTones <evilcreeper64@gmail.com>
2025-01-09 17:23:54 -0600

    textureless items

--- src/outerlimits/OuterLimits.java
+++ src/outerlimits/OuterLimits.java
@@ -8,6 +8,7 @@ import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
+import outerlimits.content.*;
 
 public class OuterLimits extends Mod{
 
@@ -17,7 +18,7 @@ public class OuterLimits extends Mod{
 
 	@Override
 	public void loadContent(){
-		Log.info("Loading some example content.");
+		OLItems.load();
 	}
 
 }
---
+++ src/outerlimits/content/OLItems.java
@@ -0,0 +1,32 @@
+package outerlimits.content;
+
+import arc.graphics.Color;
+import mindustry.type.Item;
+
+public class OLItems {
+
+	//Load Mod Items
+
+	public static Item
+	crushedIce, resonantCluster,
+	mineralDust, richMineralDust;
+
+	public static void load() {
+		crushedIce = new Item("crushed-ice") {{
+			color = Color.white;
+		}};
+
+		resonantCluster = new Item("resonant-cluster") {{
+			color = Color.white;
+			charge = 15f;
+		}};
+
+		mineralDust = new Item("mineral-dust") {{
+			color = Color.white;
+		}};
+
+		richMineralDust = new Item("rich-mineral-dust") {{
+			color = Color.white;
+		}};
+	}
+}
